074f55f733a30427ae33e768684914dd0bc81e0f
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-07-31 09:27:27 -0300

    Update Rocha.java

--- src/Objects/Rocha.java
+++ src/Objects/Rocha.java
@@ -1,23 +1,24 @@
-package Objects;
-
+package objects;
 import entities.Robo;
 import exception.*;
+import visual.*;
+
+import java.util.Arrays;
 
 public class Rocha extends Obstaculo {
-	
-	public Rocha(int[] Coord) {
-		this.coord = Coord;
-	}
-	
-	@Override
-	public void bater(Robo r) {
-		/* *
-		 	essa função só vai ser chamada quando ele de fato bater...	 
-		/* */
-			if(r.getCoord() == coord) {
-				r.setCoord(r.precoord);
-			}
-		
-	}
 
+    public Rocha(int[] Coord) {
+        this.coord = Coord;
+    }
+
+    @Override
+    public void bater(Robo r, int i) {
+		// essa função só vai ser chamada quando ele de fato bater
+        if(Arrays.equals(r.getCoord(), coord)) {
+            Visual.removerRobo(r.getCoord(0), r.getCoord(1), r.getColor());
+            r.setCoord(r.precoord);
+            Visual.addRobo(r.getCoord(0), r.getCoord(1), r.getColor());
+            Visual.bateuPedra(r.getColor());
+        }
+    }
 }
