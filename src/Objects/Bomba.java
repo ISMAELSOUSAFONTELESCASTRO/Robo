b7bf23517b3e59564564cf3ee487a5f070898cf9
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-07-31 09:25:29 -0300

    Update Bomba.java

--- src/Objects/Bomba.java
+++ src/Objects/Bomba.java
@@ -1,21 +1,27 @@
 package Objects;
 import entities.*;
+import visual.Visual;
+
+import java.util.Arrays;
 
 public class Bomba extends Obstaculo{
-	 public boolean isAtiva;
-	
-	public Bomba(int [] coord) {
-		isAtiva = true;
-		this.coord = coord;
-	}
-	
-	public void bater(Robo r) {
-		if(isAtiva == true){
-		if(r.getCoord() == coord) {
-			r.isAlive = false;
-			isAtiva = false;
-		}
-		}
-	}
-	
+    public boolean isAtiva;
+
+    public Bomba(int [] coord) {
+        isAtiva = true;
+        this.coord = coord;
+    }
+
+    public void bater(Robo r, int i) {
+        if(isAtiva){
+            if(Arrays.equals(r.getCoord(), coord)) {
+                Visual.removerRobo(r.getCoord(0), r.getCoord(1), r.getColor());
+                r.isAlive = false;
+                isAtiva = false;
+                Visual.removerBomba(coord[0], coord[1], i);
+                coord = null;
+                Visual.morreu(r.getColor());
+            }
+        }
+    }
 }
